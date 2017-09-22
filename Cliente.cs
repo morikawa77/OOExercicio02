692f80890f09b1ac2f5d08433ede0821cefd6e6b
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-09-22 16:55:32 -0300

    Inserindo encapsulamento

--- Cliente.cs
+++ Cliente.cs
@@ -3,10 +3,69 @@ namespace OOExercicio02
 {
     public class Cliente
     {
-        public Cliente()
+        public Cliente(Int32 codigo, string nome, string email, string cidade, string uf)
         {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Email = email;
+            this.Cidade = cidade;
+            this.Uf = uf;
         }
-        public Int32 Codigo;
-        public string Nome, Email, Cidade, UF;
+        private Int32 codigo;
+        private string nome, email, cidade, uf;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Uf { get => uf; set => uf = value; }
+
+
+        /*
+        public Int32 GetCodigo(){
+            return Codigo;
+        }
+
+        public void SetCodigo (Int32 Codigo){
+            this.Codigo = Codigo;
+        }
+
+        public string GetNome(){
+            return Nome;
+        }
+
+		public void SetNome(string Nome)
+		{
+			this.Nome = Nome;
+		}
+
+        public string GetEmail(){
+            return Email;
+        }
+
+		public void SetEmail(string Email)
+		{
+			this.Email = Email;
+		}
+
+        public string GetCidade(){
+            return Cidade;
+        }
+
+		public void SetCidade(string Cidade)
+		{
+			this.Cidade = Cidade;
+		}
+
+        public string GetUF(){
+            return UF;
+        }
+
+		public void SetUF(string UF)
+		{
+			this.UF = UF;
+		}
+		*/
+
     }
 }
--- Pedido.cs
+++ Pedido.cs
@@ -3,12 +3,79 @@ namespace OOExercicio02
 {
     public class Pedido
     {
-        public Pedido()
+        public Pedido(Int32 codigo, Int32 clienteCodigo, DateTime dataHora, string observacoes, Decimal valor)
         {
+            this.Codigo = codigo;
+            this.ClienteCodigo = clienteCodigo;
+            this.DataHora = dataHora;
+            this.Observacoes = observacoes;
+            this.Valor = valor;
         }
-        public Int32 Codigo, ClienteCodigo;
-        public DateTime DataHora;
-        public string Observacoes;
-        public Decimal valor;
+        private Int32 codigo, clienteCodigo;
+        private DateTime dataHora;
+        private string observacoes;
+        private Decimal valor;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public int ClienteCodigo { get => clienteCodigo; set => clienteCodigo = value; }
+        public DateTime DataHora { get => dataHora; set => dataHora = value; }
+        public string Observacoes { get => observacoes; set => observacoes = value; }
+        public decimal Valor { get => valor; set => valor = value; }
+
+
+
+
+        /*
+		public Int32 GetCodigo()
+		{
+			return Codigo;
+		}
+
+		public void SetCodigo(Int32 Codigo)
+		{
+			this.Codigo = Codigo;
+		}
+
+		public Int32 GetClienteCodigo()
+		{
+			return ClienteCodigo;
+		}
+
+		public void SetClienteCodigo(Int32 ClienteCodigo)
+		{
+			this.ClienteCodigo = ClienteCodigo;
+		}
+
+		public DateTime GetDataHora()
+		{
+			return DataHora;
+		}
+
+		public void SetDataHora(DateTime DataHora)
+		{
+			this.DataHora = DataHora;
+		}
+
+		public string GetObservacoes()
+		{
+			return Observacoes;
+		}
+
+		public void SetObservacoes(string Observacoes)
+		{
+			this.Observacoes = Observacoes;
+		}
+
+		public Decimal GetValor()
+		{
+			return Valor;
+		}
+
+		public void SetValor(decimal Valor)
+		{
+			this.Valor = Valor;
+		}
+        
+        */
     }
 }
